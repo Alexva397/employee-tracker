46d9a8af9809cf7e2c90324607edee2414245b28
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-27 01:32:54 -0500

    created schemas file to creat DB, created first prompt for employee tracker and started switch

--- employeeTracker.js
+++ employeeTracker.js
@@ -18,15 +18,52 @@ const connection = mysql.createConnection({
 
   connection.connect((err) => {
     if (err) throw err;
+    figlet('Employee \nManager', (err, data) => {
+      if (err) throw err;
+      console.log(data, '\n\n--------------------------------------------------------\n\n');
+    });
     start();
   });
 
   const start = () => {
-      figlet('Employee \nManager', (err, data) => {
-        if (err) throw err;
-        console.log(data);
+      inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View All Employees',
+          'View All Employees By Department',
+          'View All Employees By Manager',
+          'Add Employee',
+          'Remove Employee', 
+          'Update Employee Role',
+          'Update Employee Manager',
+          'Exit',
+        ],
+      })
+      .then((answers) => {
+        switch (answers.action) {
+          case 'View All Employees':
+            break;
+          case 'View All Employees By Department':
+            break;
+          case 'View All Employees By Manager':
+            break;
+          case 'Add Employee':
+            break;
+          case 'Remove Employee':
+            break;
+          case 'Update Employee Role':
+            break;
+          case 'Update Employee Manager':
+            break;
+          case 'Exit':
+            connection.end();
+            break;
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            connection.end();
+            break;
+        }
       });
-//       inquirer.prompt([
-
-//       ])
   }
