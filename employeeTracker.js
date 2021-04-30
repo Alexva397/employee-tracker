075beafefd132ea07959a66b5fc6ce2133b50e3a
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-29 23:02:47 -0500

    Created the view by department function, slightly modified query from view all, added where clause

--- employeeTracker.js
+++ employeeTracker.js
@@ -4,112 +4,143 @@ const cTable = require('console.table');
 // var figlet = require('figlet');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-  
-    // MySQL local instance
-    port: 3306,
-  
-    // Your username
-    user: 'root',
-  
-    // Be sure to update with your own MySQL password!
-    password: 'Howardthedog8675309!@',
-    database: 'employeeDB',
-  });
+  host: 'localhost',
 
-  connection.connect((err) => {
-    if (err) throw err;
-        // figlet('Employee \nManager', (err, data) => {
-    //   if (err) throw err;
-    //   console.log(data);
-    // });
-    console.log('Employee Manager');
-    start();
-  });
+  // MySQL local instance
+  port: 3306,
 
-  const start = () => {
-    console.log('\n\n---------------------------------------------------------------\n\n');
-      inquirer.prompt({
-        name: 'action',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-          'View All Employees',
-          'View All Employees By Department',
-          'View All Employees By Manager',
-          'Add Employee',
-          'Remove Employee', 
-          'Update Employee Role',
-          'Update Employee Manager',
-          'Exit',
-        ],
-      })
-      .then((answers) => {
-        switch (answers.action) {
-          case 'View All Employees':
-            viewAllEmp();
-            break;
-          case 'View All Employees By Department':
-            viewByDept();
-            break;            
-          case 'View All Employees By Manager':
-            viewByManager();
-            break;
-          case 'Add Employee':
-            addEmp();
-            break;
-          case 'Remove Employee':
-            remEmp();
-            break;
-          case 'Update Employee Role':
-            updateEmpRole();
-            break;
-          case 'Update Employee Manager':
-            updateEmpManager();
-            break;
-          case 'Exit':
-            connection.end();
-            break;
-          default:
-            console.log(`Invalid action: ${answer.action}`);
-            connection.end();
-            break;
-        }
-      });
-  }
+  // Your username
+  user: 'root',
 
-  const viewAllEmp = () => {
-    const query = `
+  // Be sure to update with your own MySQL password!
+  password: 'Howardthedog8675309!@',
+  database: 'employeeDB',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  // figlet('Employee \nManager', (err, data) => {
+  //   if (err) throw err;
+  //   console.log(data);
+  // });
+  console.log('Employee Manager');
+  start();
+});
+
+const start = () => {
+  console.log('\n\n---------------------------------------------------------------\n\n');
+  inquirer.prompt({
+    name: 'main',
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Employees',
+      'View All Employees By Department',
+      'View All Employees By Manager',
+      'Add Employee',
+      'Remove Employee',
+      'Update Employee Role',
+      'Update Employee Manager',
+      'Exit',
+    ],
+  })
+    .then((answers) => {
+      switch (answers.main) {
+        case 'View All Employees':
+          viewAllEmp();
+          break;
+        case 'View All Employees By Department':
+          viewByDept();
+          break;
+        case 'View All Employees By Manager':
+          viewByManager();
+          break;
+        case 'Add Employee':
+          addEmp();
+          break;
+        case 'Remove Employee':
+          remEmp();
+          break;
+        case 'Update Employee Role':
+          updateEmpRole();
+          break;
+        case 'Update Employee Manager':
+          updateEmpManager();
+          break;
+        case 'Exit':
+          connection.end();
+          break;
+        default:
+          console.log(`Invalid action: ${answers.main}`);
+          connection.end();
+          break;
+      }
+    });
+}
+
+const viewAllEmp = () => {
+  const query = `
     SELECT employee.id AS ID, employee.first_name, employee.last_name, role.title AS Title, department.name AS Department, CONCAT('$ ', role.salary) AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
     FROM employee
     LEFT JOIN role ON employee.role_id = role.id
     LEFT JOIN department ON role.department_id = department.id
     LEFT JOIN employee manager ON manager.id = employee.manager_id`;
 
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-      // console.log(res);
-      console.table(res);
-      start();
-    });
-  };
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    // console.log(res);
+    console.table(res);
+    start();
+  });
+};
 
-  const viewByDept = () => {
+const viewByDept = () => {
+  inquirer.prompt({
+    name: 'viewDept',
+    type: 'list',
+    message: 'Please select the department you would like to view.',
+    choices: [
+      'Administration & Management',
+      'Finance & Accounting',
+      'Marketing & Sales',
+      'Research & Development',
+      'Human Resources',
+      'Production & Manufacturing',
+      'Operations',
+      'Information Technologies',
+    ],
+  })
+    .then((answers) => {
+      const query = `
+      SELECT 
+      employee.id AS ID, employee.first_name, employee.last_name, role.title AS Title, department.name AS Department, CONCAT('$ ', role.salary) AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+      FROM employee
+      LEFT JOIN role ON employee.role_id = role.id
+      LEFT JOIN employee manager ON manager.id = employee.manager_id
+      LEFT JOIN department ON role.department_id = department.id WHERE name = ?`;
+      connection.query(query, (answers.viewDept), (err, res) => {
+        if (err) throw err;
+        // console.log(res);
+        console.table(res);
+        start();
+      });
+    })
 
-  };
-  
-  const viewByManager = () => {
-    
-  };
-  const addEmp = () => {
-    
-  };
-  const remEmp = () => {
-    
-  };
-  const updateEmpRole = () => {
-    
-  };
-  const updateEmpManager = () => {
-    
-  };
+};
+
+const viewByManager = () => {
+  // inquirer.prompt({})
+
+};
+const addEmp = () => {
+
+};
+const remEmp = () => {
+
+};
+const updateEmpRole = () => {
+
+};
+const updateEmpManager = () => {
+
+};
